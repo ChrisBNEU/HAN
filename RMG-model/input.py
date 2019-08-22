22dbe11299c5ec10a9491c66f2e1ca0d5bf49fac
Richard West <r.west@northeastern.edu>
2019-08-22 16:34:21 -0400

    Initial RMG input file

---
+++ RMG-model/input.py
@@ -0,0 +1,259 @@
+# Data sources
+database(
+    thermoLibraries=['surfaceThermoPt', 
+                     'primaryThermoLibrary',
+                     'thermo_DFT_CCSDTF12_BAC',
+                     'DFT_QCI_thermo'
+                     ],
+    reactionLibraries = [('Surface/CPOX_Pt/Deutschmann2006', True),
+                         'BurkeH2O2inN2'],
+    seedMechanisms = [],
+    kineticsDepositories = ['training'],
+    kineticsFamilies =['surface','default'],
+    kineticsEstimator = 'rate rules',
+)
+catalystProperties(
+    bindingEnergies = { # default values for Pt(111)
+                       'C':(-6.750, 'eV/molecule'),
+                       'O':(-3.586, 'eV/molecule'),
+                       'N':(-4.352, 'eV/molecule'),
+		               'H':(-2.479, 'eV/molecule'),
+                       },
+    surfaceSiteDensity=(2.9e-9, 'mol/cm^2'),
+)
+
+# List of species
+species(
+    label='X',
+    reactive=True,
+    structure=adjacencyList("1 X u0"),
+)
+
+species(
+    label='CH4',
+    reactive=True,
+    structure=SMILES("[CH4]"),
+)
+species(
+   label='O2',
+   reactive=True,
+   structure=adjacencyList(
+       """
+1 O u1 p2 c0 {2,S}
+2 O u1 p2 c0 {1,S}
+"""),
+)
+
+species(
+    label='Ar',
+    reactive=False,
+    structure=SMILES("[Ar]"),
+)
+
+species(
+    label='CO2',
+    reactive=True,
+    structure=SMILES("O=C=O"),
+)
+
+species(
+    label = 'NH2NHOOH',
+    reactive = True,
+    structure=SMILES('NNOO')
+)
+species(
+    label='H2O',
+    reactive=True,
+    structure=SMILES("O"),
+)
+
+species(
+    label='H2',
+    reactive=True,
+    structure=SMILES("[H][H]"),
+)
+
+species(
+    label='CO',
+    reactive=True,
+    structure=SMILES("[C-]#[O+]"),
+)
+
+species(
+    label='C2H6',
+    reactive=True,
+    structure=SMILES("CC"),
+)
+
+species(
+    label='CH2O',
+    reactive=True,
+    structure=SMILES("C=O"),
+)
+
+species(
+    label='CH3',
+    reactive=True,
+    structure=SMILES("[CH3]"),
+)
+
+species(
+    label='C3H8',
+    reactive=True,
+    structure=SMILES("CCC"),
+)
+
+species(
+    label='H',
+    reactive=True,
+    structure=SMILES("[H]"),
+)
+
+species(
+    label='C2H5',
+    reactive=True,
+    structure=SMILES("C[CH2]"),
+)
+
+species(
+    label='CH3OH',
+    reactive=True,
+    structure=SMILES("CO"),
+)
+
+species(
+    label='HCO',
+    reactive=True,
+    structure=SMILES("[CH]=O"),
+)
+
+species(
+    label='CH3CHO',
+    reactive=True,
+    structure=SMILES("CC=O"),
+)
+
+species(
+    label='OH',
+    reactive=True,
+    structure=SMILES("[OH]"),
+)
+
+species(
+    label='C2H4',
+    reactive=True,
+    structure=SMILES("C=C"),
+)
+
+#----------
+# Reaction systems
+surfaceReactor(
+    temperature=(400,'K'),
+    initialPressure=(1.0, 'bar'),
+    initialGasMoleFractions={
+        "CH4": 0.041866,
+        "O2": 0.03488,
+        "Ar": 0.131246,
+        'NH2NHOOH': 0.04,
+    },
+    initialSurfaceCoverages={
+        "X": 1.0,
+    },
+    surfaceVolumeRatio=(1.e5, 'm^-1'),
+    terminationConversion = { "NH2NHOOH":0.95,},
+    terminationTime=(10., 's'),
+#    terminationConversion={'O2': 0.99,},
+    terminationRateRatio=0.01
+)
+
+surfaceReactor(
+    temperature=(400,'K'),
+    initialPressure=(1.0, 'bar'),
+    initialGasMoleFractions={
+        "CH4": 0.108574,
+        "O2": 0.02088,
+        "Ar": 0.78547,
+        'NH2NHOOH': 0.1,
+    },
+    initialSurfaceCoverages={
+        "X": 1.0,
+    },
+    surfaceVolumeRatio=(1.e5, 'm^-1'),
+    terminationConversion = { "NH2NHOOH":0.95,},
+    terminationTime=(10., 's'),
+#    terminationConversion={'O2': 0.99,},
+    terminationRateRatio=0.01
+)
+
+surfaceReactor(
+    temperature=(1000,'K'),
+    initialPressure=(1.0, 'bar'),
+    initialGasMoleFractions={
+        "CH4": 0.041866,
+        "O2": 0.03488,
+        "Ar": 0.131246,
+        'NH2NHOOH': 0.04,
+    },
+    initialSurfaceCoverages={
+        "X": 1.0,
+    },
+    surfaceVolumeRatio=(1.e5, 'm^-1'),
+    terminationConversion = { "NH2NHOOH":0.95,},
+    terminationTime=(10., 's'),
+#    terminationConversion={'O2': 0.99,},
+    terminationRateRatio=0.01
+)
+    
+surfaceReactor(
+    temperature=(800,'K'),
+    initialPressure=(1.0, 'bar'),
+    initialGasMoleFractions={
+        "CH4": 0.108574,
+        "O2": 0.02088,
+        "Ar": 0.78547,
+        'NH2NHOOH': 0.1,
+    },  
+    initialSurfaceCoverages={
+        "X": 1.0,
+    },
+    surfaceVolumeRatio=(1.e5, 'm^-1'),
+    terminationConversion = { "NH2NHOOH":0.95,},
+    terminationTime=(10., 's'),
+#    terminationConversion={'O2': 0.99,},
+    terminationRateRatio=0.01
+)
+
+simulator(
+    atol=1e-18,
+    rtol=1e-12,
+)
+
+model(
+    toleranceKeepInEdge=0.0,
+    toleranceMoveToCore=1e-2,
+# inturrupt tolerance was 0.1 wout pruning, 1e8 w pruning on
+    toleranceInterruptSimulation=1e8,
+    maximumEdgeSpecies=50000,
+# PRUNING: uncomment to prune
+    minCoreSizeForPrune=50,
+# prune before simulation based on thermo
+    toleranceThermoKeepSpeciesInEdge=0.5,
+# prune rxns from edge that dont move into core
+    minSpeciesExistIterationsForPrune=2,
+# FILTERING: set so threshold is slightly larger than max rate constants
+#    filterReactions=True,
+#    filterThreshold=5e8, # default value
+)
+
+options(
+    units='si',
+    saveRestartPeriod=None,
+    generateOutputHTML=True,
+    generatePlots=False,
+    saveEdgeSpecies=False,
+    saveSimulationProfiles=True,
+)
+
+generatedSpeciesConstraints(
+    allowed=['input species','reaction libraries'],
+)
