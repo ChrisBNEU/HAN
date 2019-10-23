8e200bfdf0258b43c7761edfd553554fc48b57be
nateharms <harms.n@husky.neu.edu>
2019-10-23 16:12:27 -0400

    Renamed files to avoid confusing imports

---
+++ RMG-model/autotst/autotst-han.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#set a job name
+#SBATCH --job-name=HAN-species
+
+#a file for job output, you can check job progress
+#SBATCH --output=log-files/HAN-species.%a.sh
+
+# a file for errors from the job
+#SBATCH --error=log-files/HAN-species.%a.sh
+
+#SBATCH -N 1
+#SBATCH -n 2
+##SBATCH --ntasks-per-node=12
+#SBATCH --partition=west
+#SBATCH --mem=5GB
+#SBATCH --exclude=c3040,c5003
+#SBATCH --array=1-168
+
+source activate rmg_env
+mkdir log-files || echo "Log directory already exists"
+python autotst.py
