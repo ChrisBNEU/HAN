06a6f5a4f6c105235ea7375ef66debc41d466168
Richard West <r.west@neu.edu>
2019-11-14 11:00:29 -0500

    Trying to get conda working

--- RMG-model/slurm.sh
+++ RMG-model/slurm.sh
@@ -10,7 +10,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks=1
 
-/shared/centos7/anaconda3/3.7/bin/conda activate rmg3
+source activate rmg3
 export RMGpy=/scratch/r.west/RMG-Py
 python $RMGpy/rmg.py -p -n4 input.py
 
