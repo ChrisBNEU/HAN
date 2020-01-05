653b8ad2d76126aaafd30aba58ac7f0bf40bd640
Richard West <r.west@northeastern.edu>
2020-01-04 21:31:33 -0500

    Collision Violators notebook.
    Good at detecting reactions that are far too fast.
    (Gas phase).

---
+++ CanteraModels/Collision_violators.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Notebook to generate collision rate violators for any RMG job while running
+
+# In[1]:
+
+
+import logging
+import numpy as np
+import os
+import sys
+
+
+# In[2]:
+
+
+import os, sys
+rmg_path = os.getenv('RMGpy')
+if rmg_path not in sys.path:
+    sys.path.append(rmg_path)
+sys.path
+
+import rmgpy
+from rmgpy.chemkin import load_chemkin_file
+from rmgpy.rmgobject import RMGObject, expand_to_dict, recursive_make_object
+
+print(f"RMG-Py Version {rmgpy.__version__}")
+print(rmgpy.__file__)
+
+
+# In[3]:
+
+
+chemkin_file = '../RMG-model/chemkin/chem_annotated-gas.inp'
+chemkin_folder = os.path.split(chemkin_file)[0]
+species_dictionary_file = os.path.join(chemkin_folder, 'species_dictionary.txt')
+transport_file = os.path.join(chemkin_folder, 'tran.dat')
+
+
+# In[4]:
+
+
+species, reactions = rmgpy.chemkin.load_chemkin_file(
+                    chemkin_file, species_dictionary_file,transport_path=transport_file,
+                    check_duplicates=False, use_chemkin_names=True,read_comments=True)
+
+
+# In[5]:
+
+
+species_dict = {rmgpy.chemkin.get_species_identifier(s): s for s in species}
+species_dict.keys()
+
+
+# In[ ]:
+
+
+
+
+
+# In[6]:
+
+
+def check_collision_limit_violation(self, t_min, t_max, p_min, p_max):
+        """
+        Warn if a core reaction violates the collision limit rate in either the forward or reverse direction
+        at the relevant extreme T/P conditions. Assuming a monotonic behaviour of the kinetics.
+        Returns a list with the reaction object and the direction in which the violation was detected.
+        """
+        conditions = [[t_min, p_min]]
+        if t_min != t_max:
+            conditions.append([t_max, p_min])
+        if self.kinetics.is_pressure_dependent() and p_max != p_min:
+            conditions.append([t_min, p_max])
+            if t_min != t_max:
+                conditions.append([t_max, p_max])
+        logging.debug("Checking whether reaction {0} violates the collision rate limit...".format(self))
+        violator_list = []
+        kf_list = []
+        kr_list = []
+        collision_limit_f = []
+        collision_limit_r = []
+        for condition in conditions:
+            if len(self.reactants) >= 2:
+                try:
+                    collision_limit_f.append(self.calculate_coll_limit(temp=condition[0], reverse=False))
+                except ValueError:
+                    continue
+                else:
+                    kf_list.append(self.get_rate_coefficient(condition[0], condition[1]))
+            if len(self.products) >= 2:
+                try:
+                    collision_limit_r.append(self.calculate_coll_limit(temp=condition[0], reverse=True))
+                except ValueError:
+                    continue
+                else:
+                    kr_list.append(self.generate_reverse_rate_coefficient().get_rate_coefficient(condition[0], condition[1]))
+        if len(self.reactants) >= 2:
+            for i, k in enumerate(kf_list):
+                if k > collision_limit_f[i]:
+                    ratio = k / collision_limit_f[i]
+                    condition = '{0} K, {1:.1f} bar'.format(conditions[i][0], conditions[i][1] / 1e5)
+                    violator_list.append([self, 'forward', ratio, condition])
+        if len(self.products) >= 2:
+            for i, k in enumerate(kr_list):
+                if k > collision_limit_r[i]:
+                    ratio = k / collision_limit_r[i]
+                    condition = '{0} K, {1:.1f} bar'.format(conditions[i][0], conditions[i][1] / 1e5)
+                    violator_list.append([self, 'reverse', ratio, condition])
+        return violator_list
+
+
+# In[7]:
+
+
+import numpy as np
+np.linalg.LinAlgError
+
+
+# In[8]:
+
+
+violators = []
+for r in reactions:
+    try:
+        v = check_collision_limit_violation(r, 150+273,1000, 1e5, 30e5)
+    except np.linalg.LinAlgError:
+        print(f"LinAlgError checking {r}")
+    if v:
+        print(r)
+        violators.extend(v)
+
+
+# In[9]:
+
+
+from rmgpy.data.kinetics import TemplateReaction, LibraryReaction
+from rmgpy.rmg.pdep import PDepReaction
+
+
+# In[10]:
+
+
+violators.sort(key = lambda violator: -violator[2])
+
+
+# In[11]:
+
+
+ratios = [np.log10(violator[2]) for violator in violators]
+from matplotlib import pyplot as plt
+plt.bar(np.arange(len(ratios)), ratios)
+
+
+# In[12]:
+
+
+len(ratios), len(reactions)
+
+
+# In[13]:
+
+
+for violator in violators[:100]:
+    rxn_string = str(violator[0])
+    direction = {'forward':'=>','reverse':'<='}[violator[1]]
+    ratio = violator[2]
+    print(f"{rxn_string.replace('<=>',direction) :48s}  {ratio :.1g}")
+
+
+# In[14]:
+
+
+from collections import Counter
+import re
+
+
+# In[18]:
+
+
+families = Counter()
+templates = Counter()
+rate_rules = Counter()
+for violator in violators:
+    rxn_string = str(violator[0])
+    kinetics = violator[0].kinetics
+    comment = ''
+    if isinstance(violator[0], TemplateReaction):
+        comment = violator[0].kinetics.comment
+        violator[0].kinetics.comment = ''  # the comment is printed better when outside of the object
+    if isinstance(violator[0], LibraryReaction):
+        comment = 'Kinetic library: {0}'.format(violator[0].library)
+    if isinstance(violator[0], PDepReaction):
+        comment = 'Network #{0}'.format(violator[0].network)
+    direction = violator[1]
+    ratio = violator[2]
+    condition = violator[3]
+    
+    m = re.search('family: (.*)', comment)
+    if m:
+        families[m.group(1)] += 1
+    n = re.search('rate rule (.*)', comment)
+    if n:
+        rate_rules[m.group(1) + n.group(1)] += 1
+    n = re.search('using template (\S+)', comment)
+    if n:
+        templates[m.group(1) + n.group(1)] += 1
+    
+    if ratio < 5:
+        continue # only print the big ones!
+        
+    print('{0}\n{1}\n{2}\nDirection: {3}\nViolation factor: {4:.2g}\n'
+                      'Violation condition: {5}\n\n'.format(rxn_string, kinetics, comment, direction,
+                                                            ratio, condition))
+    if isinstance(violator[0], TemplateReaction):
+        # although this is the end of the run, restore the original comment
+        violator[0].kinetics.comment = comment
+
+
+# In[19]:
+
+
+families.most_common(10)
+
+
+# In[21]:
+
+
+# This is what was used
+templates.most_common(10)
+
+
+# In[20]:
+
+
+# This is what it was trying to use
+rate_rules.most_common(10)
+
+
+# In[ ]:
+
+
+
+
