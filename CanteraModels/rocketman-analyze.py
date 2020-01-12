674827913e1c6d22fa2e0669cce49f637d737227
Richard West <r.west@northeastern.edu>
2020-01-12 00:43:09 -0500

    Rocketman analysis notebook better

--- CanteraModels/rocketman-analyze.py
+++ CanteraModels/rocketman-analyze.py
@@ -6,7 +6,7 @@
 # ![caption](Graphics/thruster-details.png)
 # 
 
-# In[3]:
+# In[1]:
 
 
 import cantera as ct
@@ -19,14 +19,14 @@ import itertools
 import pandas as pd
 
 
-# In[4]:
+# In[2]:
 
 
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 
 
-# In[51]:
+# In[3]:
 
 
 import json
@@ -36,13 +36,13 @@ cat_area_per_vol_options = list(sorted(set(np.array(settings)[:,0])))
 temperature_c_options = list(sorted(set(np.array(settings)[:,1])))
 
 
-# In[53]:
+# In[4]:
 
 
 cat_area_per_vol_options, temperature_c_options
 
 
-# In[54]:
+# In[5]:
 
 
 
@@ -62,7 +62,7 @@ for i,(a,t) in enumerate(settings):
     setting_dict[a][t] = i
 
 
-# In[28]:
+# In[6]:
 
 
 data_dict = defaultdict(dict)
@@ -78,23 +78,65 @@ for i,(a,t) in enumerate(settings):
     data_dict[a][t] = data
 
 
-# In[33]:
+# In[7]:
+
+
+gas=ct.Solution(cti_file)
+surf = ct.Interface(cti_file,'surface1', [gas])
+
+
+# In[8]:
+
+
+print(", ".join(gas.species_names))
+
+
+# In[9]:
+
+
+print(", ".join(surf.species_names))
+
+
+# In[10]:
+
+
+# unit conversion factors to SI
+cm = 0.01 # m
+minute = 60.0  # s
+
+
+# In[11]:
+
+
+#######################################################################
+# Input Parameters for combustor
+#######################################################################
+mass_flow_rate =  0.5e-3 # kg/s
+
+pressure = ct.one_atm # constant
+
+length = 1.1 * cm  # Catalyst bed length. 11mm
+cross_section_area = np.pi * (0.9*cm)**2  # Catalyst bed area.  18mm diameter circle.
+
+
+# In[14]:
 
 
 NReactors = 2001
 def xlabels():
-    plt.xticks([0,NReactors/4,NReactors/2,3*NReactors/4, NReactors],['0','','','',f'{length*1000:.0f} mm'])
+    plt.xlim(0,50)
+    #plt.xticks([0,NReactors/4,NReactors/2,3*NReactors/4, NReactors],['0','','','',f'{length*1000:.0f} mm'])
     plt.xlabel("Distance down reactor")
 
 
-# In[40]:
+# In[16]:
 
 
 def f(cat_area_per_vol, temperature_c):
     
     print(f"Catalyst area per volume {cat_area_per_vol :.2e} m2/m3")
     print(f"Initial temperature {temperature_c :.1f} ºC")
-
+    print(f"Simulation number {setting_dict[cat_area_per_vol][temperature_c]}")
     data = data_dict[cat_area_per_vol][temperature_c]
     
     data['T (C)'].plot()
@@ -147,49 +189,7 @@ t = widgets.SelectionSlider(
 interact(f, cat_area_per_vol=a, temperature_c=t)
 
 
-# In[4]:
-
-
-gas=ct.Solution(cti_file)
-surf = ct.Interface(cti_file,'surface1', [gas])
-
-
-# In[7]:
-
-
-print(", ".join(gas.species_names))
-
-
-# In[8]:
-
-
-print(", ".join(surf.species_names))
-
-
-# In[18]:
-
-
-# unit conversion factors to SI
-cm = 0.01 # m
-minute = 60.0  # s
-
-
-# In[19]:
-
-
-#######################################################################
-# Input Parameters for combustor
-#######################################################################
-mass_flow_rate =  0.5e-3 # kg/s
-#temperature_c = 550.0  # Initial Temperature in Celsius
-print(f"Initial temperature {temperature_c :.1f} ºC")
-pressure = ct.one_atm # constant
-
-length = 1.1 * cm  # Catalyst bed length. 11mm
-cross_section_area = np.pi * (0.9*cm)**2  # Catalyst bed area.  18mm diameter circle.
-
-
-# In[18]:
+# In[ ]:
 
 
 def report_rates(n=8):
@@ -218,7 +218,7 @@ def report_rates(n=8):
 report_rates()
 
 
-# In[19]:
+# In[ ]:
 
 
 def report_rate_constants(n=8):
@@ -245,13 +245,13 @@ report_rate_constants()
 
 
 
-# In[21]:
+# In[ ]:
 
 
 sim.time
 
 
-# In[22]:
+# In[ ]:
 
 
 gas.TDY = TDY
@@ -259,19 +259,19 @@ r.syncState()
 r.thermo.T
 
 
-# In[23]:
+# In[ ]:
 
 
 r.thermo.X - gas.X
 
 
-# In[24]:
+# In[ ]:
 
 
 report_rate_constants()
 
 
-# In[29]:
+# In[ ]:
 
 
 def xlabels():
@@ -279,7 +279,7 @@ def xlabels():
     plt.xlabel("Distance down reactor")
 
 
-# In[30]:
+# In[ ]:
 
 
 data['T (C)'].plot()
@@ -287,7 +287,7 @@ plt.ylabel('T (C)')
 xlabels()
 
 
-# In[31]:
+# In[ ]:
 
 
 data[['NH2OH(3)', 'HNO3(4)', 'CH3OH(5)']].plot()
@@ -295,20 +295,20 @@ plt.ylabel('Mole fraction')
 xlabels()
 
 
-# In[32]:
+# In[ ]:
 
 
 list(data.columns)[:4]
 
 
-# In[33]:
+# In[ ]:
 
 
 data[['T (C)', 'alpha']].plot()
 xlabels()
 
 
-# In[34]:
+# In[ ]:
 
 
 ax1 = data['T (C)'].plot()
@@ -325,13 +325,13 @@ plt.savefig('temperature-and-alpha.pdf')
 plt.show()
 
 
-# In[35]:
+# In[ ]:
 
 
 data.columns
 
 
-# In[36]:
+# In[ ]:
 
 
 data[['gas_heat','surface_heat']].plot()
@@ -341,7 +341,7 @@ plt.savefig('gas_and_surface_heat.pdf')
 plt.show()
 
 
-# In[37]:
+# In[ ]:
 
 
 ax1 = data[['gas_heat','surface_heat']].plot()
@@ -359,7 +359,7 @@ plt.savefig('heats-and-alpha.pdf')
 plt.show()
 
 
-# In[38]:
+# In[ ]:
 
 
 data[['T (C)']].plot()
@@ -370,20 +370,20 @@ plt.savefig('temperature.pdf')
 plt.show()
 
 
-# In[39]:
+# In[ ]:
 
 
 data[['alpha']].plot(logy=True)
 xlabels()
 
 
-# In[40]:
+# In[ ]:
 
 
 data.plot(x='T (C)',y='alpha')
 
 
-# In[41]:
+# In[ ]:
 
 
 specs = list(data.columns)
@@ -395,13 +395,13 @@ adsorbates = [s for s in specs if 'X' in s]
 gas_species, adsorbates
 
 
-# In[42]:
+# In[ ]:
 
 
 data[gas_species[0:5]].plot(logy=True, logx=True)
 
 
-# In[43]:
+# In[ ]:
 
 
 for i in range(0,len(gas_species),10):
