f832dd479bf4f39fa4a7d712918fe1bf09f96759
Richard West <r.west@northeastern.edu>
2020-01-11 19:28:13 -0500

    Revert "removing autotst-han-library as it has that fake link thing"
    The autotst-han-library is now in the RMG-database repo (on the han branch).
    This reverts commit 5c205c9f2b2d7d478978e215960573491152a2fd.

--- RMG-model/input.py
+++ RMG-model/input.py
@@ -8,7 +8,7 @@ database(
                      'Nitrogen_G4',
                      'NitrogenCurran',
                      'primaryNS',
-                     # fixme richie: 'autotst-han-library',
+                     'autotst-han-library',
                     ],
     reactionLibraries = [('Surface/CPOX_Pt/Deutschmann2006', True),  # when Ni is used, change the library to Surface/Deutschmann_Ni
                          'BurkeH2O2inN2',
