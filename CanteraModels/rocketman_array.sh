895504f27ae10e7d386c23bc58e1d5ccb82aa9a6
Richard West <r.west@neu.edu>
2020-01-11 20:47:10 -0500

    Fixes to the rocketman_array slurm srcipt

--- CanteraModels/rocketman_array.sh
+++ CanteraModels/rocketman_array.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
+#SBATCH --array=0-35%24
 #SBATCH --nodes=1
-#SBATCH --time=00:59:59
-#SBATCH --mem=30Gb
-#SBATCH --job-name=rocket
-#SBATCH --output=rocketman/%a/slurm.log
-#SBATCH --partition=general
-#SBATCH --exclude=c[5003]
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
-#SBATCH --array=35%24
+#SBATCH --time=00:29:59
+#SBATCH --mem=8Gb
+#SBATCH --job-name=rocket
+#SBATCH --partition=west
+#SBATCH --output=rocketman-%a-slurm.log
+##SBATCH --exclude=c[5003]
+
 
 source activate rmg3
 export RMGpy=/scratch/r.west/RMG-Py
