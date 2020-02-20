871e61fd51d060e3aff5d3992f4310378b58993a
chris bais <blais.ch@husky.neu.edu>
2020-02-20 13:05:08 -0500

    Running model core 153 species and 1418 reactions with emily's surface updates

--- RMG-model/input.py
+++ RMG-model/input.py
@@ -22,23 +22,28 @@ database(
     kineticsFamilies =['default', 
 	# specifying exactly which surface families we want to use
 	# can also just use 'surface' for default surface families
-        'Surface_Adsorption_Single',
-        'Surface_Adsorption_vdW',
-        'Surface_Adsorption_Dissociative',
-        'Surface_Dissociation',
         'Surface_Abstraction',
-        #'Surface_EleyRideal_Addition_Multiple_Bond',
-        #'Surface_Migration',
-        #'Surface_Dissociation_Double_vdW',
-        #'Surface_Addition_Single_vdW',
-        #'Surface_Dissociation_vdW',
-        #'Surface_Abstraction_vdW',
-        #'Surface_Dual_Adsorption_vdW',
-        # might still be broken: 'Surface_Recombination_Single',
-        #'Surface_Adsorption_Abstraction_vdW',
-        ],
+	'Surface_Abstraction_vdW',
+	'Surface_Addition_Single_vdW',
+	'Surface_Adsorption_Abstraction_vdW',
+	#'Surface_Adsorption_Bidentate',
+	'Surface_Adsorption_Dissociative',
+	'Surface_Adsorption_Double',
+	'Surface_Adsorption_Single',
+	'Surface_Adsorption_vdW',
+	#'Surface_Bidentate_Dissociation',
+	'Surface_Dissociation',
+	'Surface_Dissociation_Double_vdW',
+	'Surface_Dissociation_vdW',
+	'Surface_Dual_Adsorption_vdW',
+	'Surface_EleyRideal_Addition_Multiple_Bond',
+	'Surface_Migration',
+	#'Surface_Recombination',
+	'Surface_Dissociation_Beta',
+	 ],
     kineticsEstimator = 'rate rules',
 )
+
 
 # Some reference values you can use below in the catalystProperties block
 bindings = {}
